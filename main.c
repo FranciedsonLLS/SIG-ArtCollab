ff050d9200c27b0f551fb3ce9e51fa13dec03e7d
FranciedsonLLS <franciedsonlucas@gmail.com>
2025-08-18 14:34:06 -0300

    TELA DE MENU DE GESTAO

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 
 // Funções 
+void nada() {
+    printf("NADA");
+};
+
 void sobre_main() {
     //LIMPAR TELA
     system("clear||cls");
@@ -31,8 +35,49 @@ void sobre_main() {
     system("clear||cls");
 }
 
-void func2() {
-    printf("FUNC2");
+void menu_main() {
+    // LIMPAR TELA
+    system("clear||cls");
+
+    int opcao;
+    do {
+    // MENU PRINCIPAL
+    printf("\n╔════════════════════════════════════════╗\n");
+    printf("║                                        ║\n");
+    printf("║              🌟 UniArt 🌟              ║\n");
+    printf("║                                        ║\n");
+    printf("╠════════════════════════════════════════╣\n");
+    printf("║  [1] ➜ Cadastro de Produtos            ║\n");
+    printf("║  [2] ➜ Gestão de Estoque               ║\n");
+    printf("║  [3] ➜ Cadastro de Colaborador         ║\n");
+    printf("║  [4] ➜ Registrar Venda                 ║\n");
+    printf("║  [5] ➜ Relatórios                      ║\n");
+    printf("║  [6] ➜ ............                    ║\n");
+    printf("║  [0] ➜ Sair                            ║\n");
+    printf("╚════════════════════════════════════════╝\n");
+    printf("Escolha uma opção: ");
+    printf("Digite sua opção: ");
+    scanf("%d", &opcao);
+    
+    switch(opcao) {
+        case 1:
+            nada();
+            break;
+        case 2:
+            nada();
+            break;
+        case 3:
+            nada();
+            break;
+        case 0:
+            printf("\nEncerrando\n");
+            break;
+        default:
+            printf("\nOpcao Invalida\n");
+        }
+
+    } while(opcao != 0);
+
     
 }
 
@@ -47,14 +92,15 @@ int main() {
         // Menu principal
         printf("\n╔════════════════════════════════════════╗\n");
         printf("║                                        ║\n");
-        printf("║         🌟 MENU PRINCIPAL 🌟           ║\n");
+        printf("║             🌟 UniArt 🌟               ║\n");
         printf("║                                        ║\n");
         printf("╠════════════════════════════════════════╣\n");
-        printf("║  [1] ➜ Func 1                          ║\n");
+        printf("║  [1] ➜ Menu                            ║\n");
         printf("║  [2] ➜ Func 2                          ║\n");
         printf("║  [3] ➜ Sobre                           ║\n");
         printf("║  [0] ➜ Sair                            ║\n");
         printf("╚════════════════════════════════════════╝\n");
+
         printf("Digite sua opção: ");
         scanf("%d", &opcao);
 
@@ -63,10 +109,10 @@ int main() {
 
         switch(opcao) {
             case 1:
-                func2();
+                menu_main();
                 break;
             case 2:
-                func2();
+                func3();
                 break;
             case 3:
                 sobre_main();
